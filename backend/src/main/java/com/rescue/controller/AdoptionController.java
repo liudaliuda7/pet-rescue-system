ea1e8cbb2fae5f12bd4c5c352bb57d3c9a2a7643
tan <1137799351@qq.com>
2026-06-01 22:33:24 +0800

    refactor(AdoptionController): 导入VisitRecord实体类
    补充导入所需的来访记录实体类依赖，完善代码导入项

--- backend/src/main/java/com/rescue/controller/AdoptionController.java
+++ backend/src/main/java/com/rescue/controller/AdoptionController.java
@@ -20,6 +20,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.rescue.entity.RescueStation;
+import com.rescue.entity.VisitRecord;
 
 @RestController
 @RequestMapping("/adoption")
